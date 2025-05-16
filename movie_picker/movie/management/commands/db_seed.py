ae208d9498f2e6d7b5deacc718b7374ee279bd92
Wojciech Krzos <wojciechkrzos67@gmail.com>
2025-05-16 22:09:02 +0200

    feat: add management commands for database seeding

---
+++ movie_picker/movie/management/commands/db_seed.py
@@ -0,0 +1,7 @@
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help: str = "bajabongo i leszczyny"
+
+    def handle(self, *args, **options) -> None:
+        self.stdout.write(self.style.SUCCESS("bajabongo i leszczyny"))
