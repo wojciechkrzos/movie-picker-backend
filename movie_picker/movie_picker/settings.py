23f49891b506751e9a7ea589dcfb9b5d5205731d
Jakub Czajkowski <and.jj.an@gmail.com>
2025-06-07 22:35:17 +0200

    feat: update CORS allowed origins for frontend development

--- movie_picker/movie_picker/settings.py
+++ movie_picker/movie_picker/settings.py
@@ -247,8 +247,12 @@ JWT_AUTH_COOKIE = 'jwt-auth'
 
 # CORS Configuration
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
+    'http://movie-picker.s.solvro.pl',
+    'https://movie-picker.s.solvro.pl',
 ]
 
 # Allow credentials in CORS requests (needed for JWT cookies)
